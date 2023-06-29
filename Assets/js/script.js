d50c3c6c8df62a4d00bc2384baefcc4a881bad9a
Vibrill306 <117178465+Vibrill306@users.noreply.github.com>
2023-06-29 13:32:51 +0530

    Add files via upload

---
+++ Assets/js/script.js
@@ -0,0 +1,160 @@
+// OWL carousel script
+$('.first-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+//  second carousel script
+
+$('.second-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:1000,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$('.third-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.fourth-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        900: {
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+// About Us Page carousel 
+
+$('.fifth-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        900: {
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$('.sixth-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+
+//  Our Services Carousel section
+
+$('.seven-carousel').owlCarousel({
+
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
